c88e15b05c27f048edd12a1626090e3c7fce558d
Saku Rautiainen <srautiainen@elara.(none)>
2015-02-26 15:31:45 +0200

    Added more constants

--- constants.py
+++ constants.py
@@ -4,3 +4,11 @@ File for all the constants
 
 @author Saku Rautiainen <saku.rautiainen@iki.fi>
 """
+
+#Database parameters
+TEMPERATURE_DATABASE_NAME = "simaattori_temperature.db"
+
+#Timer, key is sensor id, value is time in seconds. How often temperature values are saved.
+SENSOR_TIMING = {"28-000006564895": 60*60}
+#Sensor information
+SENSOR_ID_LOCATIONS = { "28-000006564895" : "Waterproof DS18B20 sensor" }
