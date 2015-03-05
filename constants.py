c8de6606bf053c3509f1ac57a00fe03227ed7637
Saku Rautiainen <saku.rautiainen@iki.fi>
2015-03-05 16:40:02 +0200

    for debug purposes timing is really short. Signed-off-by: Saku Rautiainen <saku.rautiainen@iki.fi>

--- constants.py
+++ constants.py
@@ -11,7 +11,7 @@ TEMPERATURE_DATABASE_NAME_DB = "simaattori_temperature.db"
 TEMPERATURE_DATABASE_NAME_JSON = "simaattori_temperature.json"
 
 #Timer, key is sensor id, value is time in seconds. How often temperature values are saved.
-SENSOR_TIMINGS = {"28-000006564895": 60*60}
+SENSOR_TIMINGS = {"28-000006564895": 3}
 #Sensor information
 SENSOR_ID_LOCATIONS = { "28-000006564895" : "Waterproof DS18B20 sensor" }
 
